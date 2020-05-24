cb4d2373d6ca1e1e5fe7e13586d573c74f107c8e
Abraham Murciano <abraham@CT-5555.localdomain>
2020-05-24 07:18:37 +0300

    removed all symlinks

---
+++ root/home/abraham/.scripts/spotify.sh
@@ -0,0 +1,8 @@
+
+if [ -z "$(pgrep spotify)" ]
+then
+	env LD_PRELOAD=/usr/lib/spotify-adblock.so spotify &
+	disown
+	sleep 1
+fi
+dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause
